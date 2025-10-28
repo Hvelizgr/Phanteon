2bd452105af3e955d764485a7bfb43fb33546078
Héctor Eduardo Véliz Girón <hvelizgr@gmail.com>
2025-10-27 22:29:44 -0600

    Agregar archivos de proyecto.

---
+++ Phanteon/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Phanteon")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Phanteon.Pages")]
