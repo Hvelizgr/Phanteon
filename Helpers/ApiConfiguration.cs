bb40f500fb21246ec15fa07642a9bb4f0dc3e4b3
Héctor Eduardo Véliz Girón <hvelizgr@gmail.com>
2025-11-14 19:49:52 -0600

    api desplegafa coneccion
    se conecto a la base de datos desplegada y se dejo se usar en local

--- Helpers/ApiConfiguration.cs
+++ Helpers/ApiConfiguration.cs
@@ -2,11 +2,9 @@ namespace Phanteon.Helpers
 {
     public static class ApiConfiguration
     {
-        // URL base de la API
-        // Para local en Android emulator, usa: http://10.0.2.2:5094
-        // Para dispositivo físico en la misma red: http://[TU_IP_LOCAL]:5094
-        // Para Windows/Desktop: http://localhost:5094
-        public static string BaseUrl { get; set; } = "http://10.0.2.2:5094";
+        // URL base de la API desplegada en Azure
+        // API de producción en Azure Web Services
+        public static string BaseUrl { get; set; } = "https://webapidevices-bnekc4e9a8dtarep.eastus2-01.azurewebsites.net";
 
         // Timeout para las peticiones HTTP
         public static TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(30);
